5774dc3d44b58abcd8b7d580a6c272f67728cce6
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-12-06 12:13:14 +0100

    replace_char, replace_text and insert_text methods for paragraph.

--- docx/text/paragraph.py
+++ docx/text/paragraph.py
@@ -241,6 +241,47 @@ class Paragraph(Parented):
         self.clear()
         self.add_run(text)
 
+    def replace_char(self, oldch, newch):
+        """
+        Replaces all occurences of oldch character with newch.
+        """
+        for run in self.runs:
+            run.text = run.text.replace(oldch, newch)
+        return self
+
+    def insert_text(self, position, new_text):
+        """
+        Inserts text at a given position.
+        """
+        runend = 0
+        runstart = 0
+        for run in self.runs:
+            runstart = runend
+            runend += len(run.text)
+            if runend >= position:
+                run.text = run.text[:(position-runstart)] \
+                           + new_text + run.text[(position-runstart):]
+                break
+        return self
+
+    def replace_text(self, old_text, new_text):
+        """
+        Replace all occurences of old_text with new_text. Keep runs formatting.
+        old_text can span multiple runs.
+        new_text is added to the run where old_text starts.
+        """
+        startpos = 0
+        while startpos < len(self.text):
+            try:
+                old_start = self.text[startpos:].index(old_text)
+                startpos = old_start + len(old_text)
+            except ValueError:
+                break
+
+            self.remove_text(start=old_start, end=startpos)\
+                .insert_text(old_start, new_text)
+        return self
+
     def strip(self):
         """
         Strips paragraph text.
@@ -259,6 +300,7 @@ class Paragraph(Parented):
                 run._r.getparent().remove(run._r)
             else:
                 break
+        return self
 
     def _insert_paragraph_before(self):
         """
