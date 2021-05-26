b1aef51053ee210587591c3b341be5492838da61
Balša Šarenac <balsa.sarenac@gmail.com>
2021-05-26 15:39:54 +0200

    fix: Remove undefined function call
    get_runs is undefined here, and basicly does exactly the same
    job as walk/iter_runs, so it is removed

--- docx/oxml/sdts.py
+++ docx/oxml/sdts.py
@@ -84,8 +84,6 @@ class CT_SdtContentBase(BaseOxmlElement):
             for child in el:
                 if child.tag == qn('w:r'):
                     yield child
-                elif child.tag in (qn('w:smartTag'),):
-                    yield from get_runs(child)
                 else:
                     yield from walk(child)
         yield from walk(self)
