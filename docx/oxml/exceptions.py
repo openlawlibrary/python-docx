c27531279c0e7b53fd5f8ee41fdeb2b07da82d7a
Tibor <ttapai@openlawlib.org>
2023-11-02 16:48:40 +0100

    fix: In heading paragraph hide "hidden text" refering to table content link.

--- docx/oxml/exceptions.py
+++ docx/oxml/exceptions.py
@@ -14,3 +14,9 @@ class InvalidXmlError(XmlchemyError):
     Raised when invalid XML is encountered, such as on attempt to access a
     missing required child element
     """
+
+class HiddenTextTC(Exception):
+    """
+    Raised when parsing hidden text that represents table content.
+    This text is used as a link to Table of Contents, and shouldn't be return as viewable text.
+    """
