e927e38f2ea4c71c9517fd3d48991e50c0926899
Tibor Tapai <tiberlas1997@gmail.com>
2024-10-09 16:04:55 +0200

    feat(table): add functions for manipulating borders and margins

--- docx/oxml/__init__.py
+++ docx/oxml/__init__.py
@@ -173,19 +173,23 @@ register_element_cls('w:uiPriority',     CT_DecimalNumber)
 register_element_cls('w:unhideWhenUsed', CT_OnOff)
 
 from .table import (  # noqa
+    CT_Border,
     CT_Height,
     CT_Row,
     CT_Tbl,
     CT_TblGrid,
     CT_TblGridCol,
     CT_TblLayoutType,
+    CT_TblCellMar,
     CT_TblPr,
     CT_TblWidth,
     CT_Tc,
+    CT_TcBorders,
     CT_TcPr,
     CT_TrPr,
     CT_VMerge,
     CT_VerticalJc,
+    MT_BorderMargin,
 )
 register_element_cls('w:bidiVisual', CT_OnOff)
 register_element_cls('w:gridCol',    CT_TblGridCol)
@@ -193,11 +197,19 @@ register_element_cls('w:gridSpan',   CT_DecimalNumber)
 register_element_cls('w:tbl',        CT_Tbl)
 register_element_cls('w:tblGrid',    CT_TblGrid)
 register_element_cls('w:tblLayout',  CT_TblLayoutType)
+register_element_cls('w:tblCellMar',  CT_TblCellMar)
 register_element_cls('w:tblPr',      CT_TblPr)
 register_element_cls('w:tblStyle',   CT_String)
+register_element_cls('w:top',         MT_BorderMargin)
+register_element_cls('w:left',         MT_BorderMargin)
+register_element_cls('w:bottom',         MT_BorderMargin)
+register_element_cls('w:right',         MT_BorderMargin)
 register_element_cls('w:tc',         CT_Tc)
+register_element_cls('w:tcBorders',         CT_TcBorders)
+register_element_cls('w:tblBorders',         CT_TcBorders)
 register_element_cls('w:tcPr',       CT_TcPr)
 register_element_cls('w:tcW',        CT_TblWidth)
+register_element_cls('w:tblW',        CT_TblWidth)
 register_element_cls('w:tr',         CT_Row)
 register_element_cls('w:trHeight',   CT_Height)
 register_element_cls('w:trPr',       CT_TrPr)
--- docx/oxml/simpletypes.py
+++ docx/oxml/simpletypes.py
@@ -185,6 +185,18 @@ class XsdUnsignedLong(BaseIntType):
         cls.validate_int_in_range(value, 0, 18446744073709551615)
 
 
+class ST_Border(XsdString):
+
+    @classmethod
+    def validate(cls, value):
+        cls.validate_string(value)
+        valid_values = ('nil', 'none', 'single', 'thick', 'double', 'dotted', 'dashed', 'dotDash', 'dotDotDash', 'triple', 'thinThickSmallGap', 'thickThinSmallGap', 'thinThickThinSmallGap', 'thinThickMediumGap', 'thickThinMediumGap', 'thinThickThinMediumGap', 'thinThickLargeGap', 'thickThinLargeGap', 'thinThickThinLargeGap', 'wave', 'doubleWave', 'dashSmallGap', 'dashDotStroked', 'threeDEmboss', 'threeDEngrave', 'outset', 'inset', 'apples', 'archedScallops', 'babyPacifier', 'babyRattle', 'balloons3Colors', 'balloonsHotAir', 'basicBlackDashes', 'basicBlackDots', 'basicBlackSquares', 'basicThinLines', 'basicWhiteDashes', 'basicWhiteDots', 'basicWhiteSquares', 'basicWideInline', 'basicWideMidline', 'basicWideOutline', 'bats', 'birds', 'birdsFlight', 'cabins', 'cakeSlice', 'candyCorn', 'celticKnotwork', 'certificateBanner', 'chainLink', 'champagneBottle', 'checkedBarBlack', 'checkedBarColor', 'checkered', 'christmasTree', 'circlesLines', 'circlesRectangles', 'classicalWave', 'clocks', 'compass', 'confetti', 'confettiGrays', 'confettiOutline', 'confettiStreamers', 'confettiWhite', 'cornerTriangles', 'couponCutoutDashes', 'couponCutoutDots', 'crazyMaze', 'creaturesButterfly', 'creaturesFish', 'creaturesInsects', 'creaturesLadyBug', 'crossStitch', 'cup', 'decoArch', 'decoArchColor', 'decoBlocks', 'diamondsGray', 'doubleD', 'doubleDiamonds', 'earth1', 'earth2', 'eclipsingSquares1', 'eclipsingSquares2', 'eggsBlack', 'fans', 'film', 'firecrackers', 'flowersBlockPrint', 'flowersDaisies', 'flowersModern1', 'flowersModern2', 'flowersPansy', 'flowersRedRose', 'flowersRoses', 'flowersTeacup', 'flowersTiny', 'gems', 'gingerbreadMan', 'gradient', 'handmade1', 'handmade2', 'heartBalloon', 'heartGray', 'hearts', 'heebieJeebies', 'holly', 'houseFunky', 'hypnotic', 'iceCreamCones', 'lightBulb', 'lightning1', 'lightning2', 'mapPins', 'mapleLeaf', 'mapleMuffins', 'marquee', 'marqueeToothed', 'moons', 'mosaic', 'musicNotes', 'northwest', 'ovals', 'packages', 'palmsBlack', 'palmsColor', 'paperClips', 'papyrus', 'partyFavor', 'partyGlass', 'pencils', 'people', 'peopleWaving', 'peopleHats', 'poinsettias', 'postageStamp', 'pumpkin1', 'pushPinNote2', 'pushPinNote1', 'pyramids', 'pyramidsAbove', 'quadrants', 'rings', 'safari', 'sawtooth', 'sawtoothGray', 'scaredCat', 'seattle', 'shadowedSquares', 'sharksTeeth', 'shorebirdTracks', 'skyrocket', 'snowflakeFancy', 'snowflakes', 'sombrero', 'southwest', 'stars', 'starsTop', 'stars3d', 'starsBlack', 'starsShadowed', 'sun', 'swirligig', 'tornPaper', 'tornPaperBlack', 'trees', 'triangleParty', 'triangles', 'tribal1', 'tribal2', 'tribal3', 'tribal4', 'tribal5', 'tribal6', 'twistedLines1', 'twistedLines2', 'vine', 'waveline', 'weavingAngles', 'weavingBraid', 'weavingRibbon', 'weavingStrips', 'whiteFlowers', 'woodwork', 'xIllusions', 'zanyTriangles', 'zigZag', 'zigZagStitch')
+        if value not in valid_values:
+            raise ValueError(
+                "must be one of %s, got '%s'" % (valid_values, value)
+            )
+
+
 class ST_BrClear(XsdString):
 
     @classmethod
